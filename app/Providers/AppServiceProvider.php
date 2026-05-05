6e60cbd820be230ab5c9d253d546a6de7e1f6bf6
slouowzee <gael.pilet1@gmail.com>
2026-05-05 11:32:26 +0200

    e6.5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        URL::forceScheme('https');
+
         Password::defaults(function () {
             return Password::min(12)
                 ->letters()
